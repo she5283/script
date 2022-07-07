8d835c3b6c6093693f7cd74c07d4d0604b396499
eosu <she5823@naver.com>
2022-07-07 20:36:03 +0900

    0707

---
+++ 07.07-2/script/main.js
@@ -0,0 +1,6 @@
+function init(){
+  $('header').load('/07.07/fragment/header.html')
+  $('nav').load('/07.07/fragment/nav.html')
+  $('aside').load('/07.07/fragment/aside.html')
+  $('footer').load('/07.07/fragment/footer.html')
+}
